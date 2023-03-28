13fc3db9c37c7d911774079b3beb143ed0a5b1f5
kayfernander2022 <kayfernander@hotmail.com>
2023-03-28 18:26:40 -0400

    fix another lint error

--- src/components/photos/index.tsx
+++ src/components/photos/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import PhotoCard from './photoCard'
 import Photo from '../../models/photos'
